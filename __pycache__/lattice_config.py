1961a6dc4c27002b6e5b3a216c6541e5ee7dd0bf
jkraason <ivjohnson31@gmail.com>
2025-10-15 12:00:48 +0100

    lattice_fix

--- __pycache__/lattice_config.py
+++ __pycache__/lattice_config.py
@@ -9,10 +9,14 @@ def lattice_positions(config,size):
         for i in range(0,(size**2)):
             x_temp = np.array([])
             y_temp = np.array([])
-            x_temp = np.append(x_temp,x[i]+1)
-            y_temp = np.append(y_temp,y[i])
-            y_temp = np.append(y_temp,y[i]+1)
-            x_temp = np.append(x_temp,x[i])
+            x_temp = np.append(x_temp,x[i]) #0
+            y_temp = np.append(y_temp,y[i]) #0
+            y_temp = np.append(y_temp,y_temp[i]+1) #0,1
+            x_temp = np.append(x_temp,x_temp[i]+1) #0,1
+            x_temp = np.append(x_temp,x[i]+1)#0,1,1
+            y_temp = np.append(y_temp, y_temp[i])#0,1,0
+            x_temp = np.append(x_temp, x_temp[i])#0,1,1,0
+            y_temp = np.append(y_temp, y_temp[i]+1)#0,1,0,1
             print(x_temp)
             print(y_temp)
             x = np.append(x,x_temp)
